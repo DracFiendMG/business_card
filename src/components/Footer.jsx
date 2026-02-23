ae9bc62c86feb52a11350f177a8f689411997bd5
DracFiendMG <sreeramrdy@gmail.com>
2026-02-23 21:57:19 +0530

    Enhance layout and styling: add social media links, improve button design, and update CSS for better responsiveness

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,10 @@
 export default function Footer() {
     return (
         <footer>
-            
+            <a href="https://x.com/SreeramReddy11" target="blank"><i class="fa-brands fa-square-twitter"></i></a>
+            <a href="#"><i class="fa-brands fa-square-facebook"></i></a>
+            <a href="#"><i class="fa-brands fa-square-instagram"></i></a>
+            <a href="https://github.com/DracFiendMG" target="blank"><i class="fa-brands fa-square-github"></i></a>
         </footer>
     )
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,19 +2,19 @@ export default function Main() {
     return (
         <main>
             <h1>Sreeram Reddy Velagala</h1>
-            <p>Full Stack Developer</p>
-            <a href="#">sreeramreddy.website</a>
-            <div>
-                <a>Email</a>
-                <a>LinkedIn</a>
+            <p className="role">Full Stack Developer</p>
+            <a className="portfolio-link" href="#">sreeramreddy.website</a>
+            <div className="buttons">
+                <a className="button email" href="mailto:sreeramrdy@gmail.com"><i class="fa-solid fa-envelope"></i>Email</a>
+                <a className="button linkedin" href="https://www.linkedin.com/in/sreeram-17ba861a4/" target="blank"><i class="fa-brands fa-square-linkedin"></i>LinkedIn</a>
             </div>
-            <div>
+            <div className="about">
                 <h3>About</h3>
                 <p>I am a full stack developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
             </div>
-            <div>
+            <div className="interests">
                 <h3>Interests</h3>
-                <p>Web Development, Open Source, AI</p>
+                <p>Web Development, Open Source, AI, Exploring New Tech, Eating</p>
             </div>
         </main>
     )
